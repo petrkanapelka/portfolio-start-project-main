41e87f90fe1ec58f3353631651dcb6a1df6409c0
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-04-25 17:28:17 +0300

    feat: add navItems

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,11 +4,12 @@ import styled from 'styled-components';
 import { Logo } from '../../components/logo/Logo';
 import { Menu } from '../../components/menu/Menu';
 
+const navItems = ['Home', 'Skills', 'Works', 'Testimony', 'Contacts']
 export const Header = () => {
     return (
         <StyledHeader>
             <Logo/>
-            <Menu/>
+            <Menu menuItems={navItems}/>
         </StyledHeader>
     );
 };
