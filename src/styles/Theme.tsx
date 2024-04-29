09a98366d2cef8e33ea366e8130ec83eeb17c052
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-04-29 15:49:30 +0300

    refactor: add property fonts to myTheme

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -3,7 +3,10 @@ export const myTheme = {
         primaryBg: "#1F1F20",
         secondaryBg: "#252527",
         accent: "#7572D5",
-
         font: "#FFFFFF"
+    },
+    fonts: {
+        primaryFamily: "'Poppins', sans-serif",
+        secondaryFamily: "'Josefin Sans', sans-serif"
     }
-}
+};
