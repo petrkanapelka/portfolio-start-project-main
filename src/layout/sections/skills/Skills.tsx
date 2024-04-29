de1b0f807fec9f1eb4158902d767c2616eb3ceb3
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-04-29 17:39:13 +0300

    fix: wrap content of Skills into Container

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -2,21 +2,24 @@ import styled from 'styled-components';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Skill } from './skill/Skill';
+import { Container } from '../../../components/Container';
 
 export const lorem = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem veniam neque adipisci facilis id dolorum aperiam corporis culpa alias beatae esse quis libero saepe perspiciatis velit facere natus, non architecto!"
 
 export const Skills = () => {
     return (
         <StyledSkills>
-            <SectionTitle>Skills</SectionTitle>
-            <FlexWrapper wrap='wrap'>
-                <Skill iconID='code' title='HTML5' text={lorem}/>
-                <Skill iconID='css' title='CSS' text={lorem}/>
-                <Skill iconID='react' title='React' text={lorem}/>
-                <Skill iconID='ts' title='Typescript' text={lorem}/>
-                <Skill iconID='sc' title='Styled Components' text={lorem}/>
-                <Skill iconID='figma' title='Web Development' text={lorem}/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>Skills</SectionTitle>
+                <FlexWrapper wrap='wrap'>
+                    <Skill iconID='code' title='HTML5' text={lorem}/>
+                    <Skill iconID='css' title='CSS' text={lorem}/>
+                    <Skill iconID='react' title='React' text={lorem}/>
+                    <Skill iconID='ts' title='Typescript' text={lorem}/>
+                    <Skill iconID='sc' title='Styled Components' text={lorem}/>
+                    <Skill iconID='figma' title='Web Development' text={lorem}/>
+                </FlexWrapper>
+            </Container>
         </StyledSkills>
     );
 };
