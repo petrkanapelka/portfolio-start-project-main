431c049e3f96e97edba4aefe93cb9b3832a6eb06
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-04-29 17:38:36 +0300

    feat: add styles for SectionTitle

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,6 +1,26 @@
 import styled from "styled-components";
+import { myTheme } from "../styles/Theme";
 
 export const SectionTitle = styled.h2`
-    color: red;
     text-align: center;
+    font-family: ${myTheme.fonts.secondaryFamily};
+    font-weight: 600;
+    font-size: 36px;
+    letter-spacing: 0.14em;
+    position: relative;
+    margin-bottom: 90px;
+
+    &::before {
+        content: '';
+        display: inline-block;
+        height: 1px;
+        width: 55px;
+        background-color: ${myTheme.colors.accent};
+
+        position: absolute;
+        left: 50% ;
+        bottom: -30px;
+        transform: translateX(-50%);
+
+    }
 `
