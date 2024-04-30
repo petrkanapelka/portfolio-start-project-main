6e0cdc9c4af9031d773d7df4f642463c64d7e47c
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-04-30 15:32:02 +0300

    style: Add margin to IconWrapper in Testimony component

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -3,13 +3,16 @@ import { SectionTitle } from '../../../components/SectionTitle';
 import { Icon } from '../../../components/icon/Icon';
 import { Slider } from '../../../components/slider/Slider';
 import { FlexWrapper } from '../../../components/FlexWrapper';
+import { IconWrapper } from '../skills/skill/Skill';
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
             <SectionTitle>Testimony</SectionTitle>
             <FlexWrapper direction={"column"}>
-                <Icon iconID='doubleQuotes'/>
+                <IconWrapper>
+                    <Icon iconID='doubleQuotes'/>
+                </IconWrapper>
                 <Slider/>
             </FlexWrapper>
         </StyledTestimony>
@@ -17,5 +20,7 @@ export const Testimony = () => {
 };
 
 const StyledTestimony = styled.section`
-    min-height: 100vh;
+    ${IconWrapper} {
+        margin: 40px 0 72px;
+    }
 `
