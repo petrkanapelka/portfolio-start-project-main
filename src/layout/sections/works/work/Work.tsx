9b01fddfbfcdc214a7a6d6e5f9100c7ec99acf13
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-04-30 12:12:03 +0300

    feat: Update Work component with additional styles and components

--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from '../../../../components/Link';
+import { myTheme } from '../../../../styles/Theme';
+import { Button } from '../../../../components/Button';
 
 type WorkPropsType = {
     title: string
@@ -10,20 +13,65 @@ type WorkPropsType = {
 export const Work = (props: WorkPropsType) => {
     return (
         <StyledWork>
-            <Image src={props.src} alt=''/>
-            <Title>{props.title}</Title>
-            <Text>{props.text}</Text>
-            <Link href=''>demo</Link>
-            <Link href=''>code</Link>
+            <ImageWrapper>
+                <Image src={props.src} alt='demo' />
+                <Button>view project</Button>
+            </ImageWrapper>
+            <Description>
+                <Title>{props.title}</Title>
+                <Text>{props.text}</Text>
+                <Link href=''>demo</Link>
+                <Link href=''>code</Link>
+            </Description>
         </StyledWork>
     );
 };
 
 
 const StyledWork = styled.div`
-    border-color: yellow;
+    border-color: ${myTheme.colors.secondaryBg};
     max-width: 540px;
     width: 100%;
+    ${Link} {
+        padding: 10px 0;
+
+        & + ${Link} {
+            margin-left: 20px;
+        }
+    }
+`
+const Description = styled.div`
+    padding: 25px 20px;
+`
+
+const ImageWrapper = styled.div`
+    position: relative;
+    &:hover {
+        &::before {
+            content: "";
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            top: 0;
+            backdrop-filter: blur(8px);
+            background: rgba(0, 0, 0, 0.3);
+        }
+        ${Button} {
+            transform: translate(-50%, -50%) scale(1);
+        }
+    }
+    ${Button} {
+        transform: scale(0);
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%) scale(0);
+        &::before {
+            width: 100%;
+            height: 100%;
+        }
+    }
 `
 
 const Image = styled.img`
@@ -31,9 +79,8 @@ const Image = styled.img`
     height: 260px;
     object-fit: cover;
 `
-
-const Link = styled.a``
-
 const Title = styled.h3``
 
-const Text = styled.p``
+const Text = styled.p`
+    margin: 14px 0 10px;
+`
