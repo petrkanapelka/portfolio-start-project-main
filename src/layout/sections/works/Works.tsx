27e6fff6fd66088ddfb3bdae31972bc91e59e587
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-04-30 12:15:43 +0300

    feat: Update Works component with TabMenu and Container

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
 import styled from 'styled-components';
 import { SectionTitle } from '../../../components/SectionTitle';
-import { Menu } from '../../../components/menu/Menu';
+import { TabMenu } from './tabMenu/TabMenu';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Work } from './work/Work';
 import socialImg from '../../../assets/images/proj-1.webp';
 import timerImg from '../../../assets/images/proj-2.webp';
 import { lorem } from '../skills/Skills';
+import { Container } from '../../../components/Container';
 
 const worksItems = ['ALL', 'LANDING PAGE', 'REACT', 'SPA'];
 
 export const Works = () => {
     return (
         <StyledWorks>
-            <SectionTitle>My works</SectionTitle>
-            <Menu menuItems={worksItems}/>
-            <FlexWrapper justify='space-around'>
-                <Work title='social Network' src={socialImg} text={lorem}></Work>
-                <Work title='Timer' src={timerImg} text={lorem}></Work>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>My works</SectionTitle>
+                <TabMenu menuItems={worksItems}/>
+                <FlexWrapper justify='space-between' align='flex-start'>
+                    <Work title='Social Network' src={socialImg} text={lorem}></Work>
+                    <Work title='Timer' src={timerImg} text={lorem}></Work>
+                </FlexWrapper>
+            </Container>
         </StyledWorks>
     );
 };
 
 const StyledWorks = styled.section`
-    min-height: 100vh;
 `
