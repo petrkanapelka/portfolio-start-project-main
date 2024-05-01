480299e340697746293a91a519e5b8192cf7951b
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-01 22:52:58 +0300

    style: Updated Footer component styles

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Icon } from '../../components/icon/Icon';
 import { FlexWrapper } from '../../components/FlexWrapper';
+import { myTheme } from '../../styles/Theme';
 
 export const Footer = () => {
     return (
@@ -11,22 +12,22 @@ export const Footer = () => {
                 <SocialList>
                     <SocialItem>
                         <SocialLink>
-                            <Icon iconID='instagram' height='21px' width='21px' viewBox='0 0 21px 21px'></Icon>
+                            <Icon iconID='instagram' height='21px' width='21px' viewBox='0 0 21 21'></Icon>
                         </SocialLink>
                     </SocialItem>
                     <SocialItem>
                         <SocialLink>
-                            <Icon iconID='vk' height='21px' width='21px' viewBox='0 0 21px 21px'></Icon>
+                            <Icon iconID='vk' height='21px' width='21px' viewBox='0 0 21 21'></Icon>
                         </SocialLink>
                     </SocialItem>
                     <SocialItem>
                         <SocialLink>
-                            <Icon iconID='telegram' height='21px' width='21px' viewBox='0 0 21px 21px'></Icon>
+                            <Icon iconID='telegram' height='21px' width='21px' viewBox='0 0 21 21'></Icon>
                         </SocialLink>
                     </SocialItem>
                     <SocialItem>
                         <SocialLink>
-                            <Icon iconID='linkedin' height='21px' width='21px' viewBox='0 0 21px 21px'></Icon>
+                            <Icon iconID='linkedin' height='21px' width='21px' viewBox='0 0 21 21'></Icon>
                         </SocialLink>
                     </SocialItem>
                 </SocialList>
@@ -37,21 +38,48 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-    background-color: aliceblue;
-    min-height: 30vh;
+    background-color: ${myTheme.colors.primaryBg};
+    padding: 40px 0;
 `
 const Name = styled.h4`
     text-align: center;
+    font-weight: 700;
+    font-size: 22px;
+    line-height: 22px;
+    letter-spacing: 3px;
 `
 
 const SocialList = styled.ul`
     display: flex;
-    gap: 30px;
+    gap: 20px;
     list-style-type: none;
+    margin: 30px 0 30px;
 `
 
-const SocialLink = styled.li``
+const SocialLink = styled.li`
+    border-radius: 50%;
+    width: 35px;
+    height: 35px;
+    background: rgba(255, 255, 255, 0.1);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${myTheme.colors.accent};
+
+    &:hover {
+        color: ${myTheme.colors.primaryBg};
+        transform: translateY(-4px);
+        background: #7572d5;
+        cursor: pointer;
+    }
+`
 
 const SocialItem = styled.div``
 
-const Copyright = styled.small``
+const Copyright = styled.small`
+    font-weight: 400;
+    font-size: 12px;
+    text-align: center;
+    color: rgba(255, 255, 255, 0.5);
+`
