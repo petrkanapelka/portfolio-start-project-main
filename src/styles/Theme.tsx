6a56d7cad42768b61d0a710f395aca1804e1dbab
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-07 22:47:57 +0300

    feat: add desktop property in media

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -15,5 +15,6 @@ export const myTheme = {
     media: {
         tablet: "screen and (max-width: 768px)",
         mobile: "screen and (max-width: 576px)",
+        desktop: "screen and (min-width: 1140px)",
     }
 };
