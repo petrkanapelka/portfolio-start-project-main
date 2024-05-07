ea4b7328b4c81f11e1e7470325f8490f43bfa977
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-07 21:16:25 +0300

    style: Added styles and layout adjustments for Main component

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,12 +4,13 @@ import styled from 'styled-components';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Container } from '../../../components/Container';
 import { myTheme } from '../../../styles/Theme';
+import { font } from '../../../styles/Common';
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper justify='space-between'>
+                <FlexWrapper justify='space-around' wrap='wrap'>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am <span>Petr Kanapelka</span></Name>
@@ -26,6 +27,7 @@ export const Main = () => {
 const PhotoWrapper = styled.div`
     position: relative;
     z-index: 0;
+    margin-top: 65px;
     &::before {
         content: "";
         width: 360px;
@@ -35,6 +37,11 @@ const PhotoWrapper = styled.div`
         top: -24px;
         left: 24px;
         z-index: -1;
+
+        @media ${myTheme.media.mobile} {
+            width: 314px;
+            height: 414px;
+        }
     }
 `
 
@@ -47,21 +54,24 @@ const StyledImg = styled.img`
     width: 350px;
     height: 430px;
     object-fit: cover;
+    margin-right: 20px;
+
+    @media ${myTheme.media.mobile} {
+        width: 310px;
+        height: 380px;
+    }
 `
 
 const MainTitle = styled.h1`
-    font-family: ${myTheme.fonts.primaryFamily};
-    font-weight: 400;
-    font-size: 27px;
+    ${font({family: myTheme.fonts.primaryFamily, weight: 400, Fmax: 27, Fmin: 20})};
 `
 const Name = styled.h1`
-    font-family: ${myTheme.fonts.secondaryFamily};
-    font-weight: 700;
-    font-size: 50px;
+    ${font({family: myTheme.fonts.secondaryFamily, weight: 700, Fmax: 50, Fmin: 36})};
     letter-spacing: 0.05em;
     margin: 10px 0;
 
     span {
+        white-space: nowrap;
         position: relative;
         z-index: 1;
         &::before {
@@ -76,10 +86,12 @@ const Name = styled.h1`
             z-index: -1;
         }
     }
+
+    @media ${myTheme.media.mobile} {
+        margin: 15px 0 22px;
+    }
 `
 
 const SmallText = styled.span`
-    font-family: ${myTheme.fonts.primaryFamily};
-    font-weight: 400;
-    font-size: 14px;
+    ${font({family: myTheme.fonts.primaryFamily, weight: 400, Fmax: 14, Fmin: 10})};
 `;
