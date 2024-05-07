d7587c780ea897c16a7c7893de1c453f1e01ad29
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-07 22:58:46 +0300

    style:  add container to Testimony component

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,17 +4,20 @@ import { Icon } from '../../../components/icon/Icon';
 import { Slider } from '../../../components/slider/Slider';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { IconWrapper } from '../skills/skill/Skill';
+import { Container } from '../../../components/Container';
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
-            <SectionTitle>Testimony</SectionTitle>
-            <FlexWrapper direction={"column"}>
-                <IconWrapper>
-                    <Icon iconID='doubleQuotes'/>
-                </IconWrapper>
-                <Slider/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>Testimony</SectionTitle>
+                <FlexWrapper direction={"column"}>
+                    <IconWrapper>
+                        <Icon iconID='doubleQuotes'/>
+                    </IconWrapper>
+                    <Slider/>
+                </FlexWrapper>
+            </Container>
         </StyledTestimony>
     );
 };
