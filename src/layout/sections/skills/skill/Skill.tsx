b1dd5d7f9589f0b3f2dd62c75bfc9af0ab56eeff
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-09 01:02:37 +0300

    refactor: update Skill to React func component

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,63 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
 import { Icon } from '../../../../components/icon/Icon';
 import { FlexWrapper } from '../../../../components/FlexWrapper';
-import { myTheme } from '../../../../styles/Theme';
+import { S } from '../Skills_Styles';
+import React from 'react';
 
 type SkillPropsType = {
     iconID: string
     title: string
     text: string
 }
-export const Skill = (props: SkillPropsType) => {
+
+export const Skill: React.FC<SkillPropsType> = (props: SkillPropsType) => {
     return (
-        <StyledSkill>
+        <S.Skill>
             <FlexWrapper direction='column' align='center'>
-                <IconWrapper>
+                <S.IconWrapper>
                     <Icon iconID={props.iconID} width='50' height='50' viewBox='0 0 50 50'></Icon>
-                </IconWrapper>
-                <SkillTitle>{props.title}</SkillTitle>
-                <SkillText>{props.text}</SkillText>
+                </S.IconWrapper>
+                <S.SkillTitle>{props.title}</S.SkillTitle>
+                <S.SkillText>{props.text}</S.SkillText>
             </FlexWrapper>
-        </StyledSkill>
+        </S.Skill>
     );
 };
 
-const StyledSkill = styled.div`
-    width: 330px;
-    flex-grow: 1;
-    padding: 40px 20px 14px;
-
-    @media ${myTheme.media.mobile} {
-        padding: 62px 0 40px;
-    }
-`
-const SkillTitle = styled.div`
-    text-transform: uppercase;
-    margin: 70px 0 15px;
-    letter-spacing: 0.06em;
-    text-transform: uppercase;
-`
-const SkillText = styled.div`
-    text-align: center;
-`
-
-export const IconWrapper = styled.div`
-    position: relative;
-    z-index: 0;
-
-    &::before {
-        content: '';
-        display: inline-block;
-        width: 80px;
-        height: 80px;
-        background: rgba(255, 255, 255, 0.1);
-        transform: rotate(-45deg) translate(-50%, -50%);
-
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform-origin: top left;
-        z-index: -1;
-    }
-`;
