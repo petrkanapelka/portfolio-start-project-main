7313a57d5cc2c8903e1e280bf4efd0f5202f3925
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-09 01:02:55 +0300

    refactor: update Skill to React func component and add mapping

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -1,29 +1,56 @@
-import styled from 'styled-components';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Skill } from './skill/Skill';
 import { Container } from '../../../components/Container';
+import { S } from './Skills_Styles';
+import React from 'react';
 
 export const lorem = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem veniam neque adipisci facilis id dolorum aperiam corporis culpa alias beatae esse quis libero saepe perspiciatis velit facere natus, non architecto!"
 
-export const Skills = () => {
+const SkillData = [
+    {
+        iconId: 'code',
+        title: 'HTML5',
+        description: lorem
+    },
+    {
+        iconId: 'css',
+        title: 'CSS3',
+        description: lorem
+    },
+    {
+        iconId: 'react',
+        title: 'React',
+        description: lorem
+    },
+    {
+        iconId: 'ts',
+        title: 'Typescript',
+        description: lorem
+    },
+    {
+        iconId: 'sc',
+        title: 'Styled Components',
+        description: lorem
+    },
+    {
+        iconId: 'figma',
+        title: 'Web Design',
+        description: lorem
+    },
+]
+
+export const Skills: React.FC = () => {
     return (
-        <StyledSkills>
+        <S.Skills>
             <Container>
                 <SectionTitle>My Skills</SectionTitle>
                 <FlexWrapper wrap='wrap'>
-                    <Skill iconID='code' title='HTML5' text={lorem}/>
-                    <Skill iconID='css' title='CSS' text={lorem}/>
-                    <Skill iconID='react' title='React' text={lorem}/>
-                    <Skill iconID='ts' title='Typescript' text={lorem}/>
-                    <Skill iconID='sc' title='Styled Components' text={lorem}/>
-                    <Skill iconID='figma' title='Web Development' text={lorem}/>
+                    {SkillData.map((skill, index) => {
+                        return <Skill key={index} iconID={skill.iconId} title={skill.title} text={skill.description}/>
+                    })}
                 </FlexWrapper>
             </Container>
-        </StyledSkills>
+        </S.Skills>
     );
 };
-
-const StyledSkills = styled.section`
-
-`
