c625ef279720c1674800eec660d3a67b19f5a3fd
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-09 21:12:24 +0300

    refactor: add mapping for functional component Works

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { TabMenu } from './tabMenu/TabMenu';
 import { FlexWrapper } from '../../../components/FlexWrapper';
@@ -7,26 +6,36 @@ import socialImg from '../../../assets/images/proj-1.webp';
 import timerImg from '../../../assets/images/proj-2.webp';
 import { lorem } from '../skills/Skills';
 import { Container } from '../../../components/Container';
+import React from 'react';
+import { S } from './work/Works_Styles';
 
 const worksItems = ['ALL', 'LANDING PAGE', 'REACT', 'SPA'];
 
-export const Works = () => {
+const workData = [
+    {
+        title: 'Social Network',
+        src: socialImg,
+        text: lorem,
+    },
+    {
+        title: 'Timer',
+        src: timerImg,
+        text: lorem,
+    },
+]
+
+export const Works: React.FC = () => {
     return (
-        <StyledWorks>
+        <S.Works>
             <Container>
                 <SectionTitle>My works</SectionTitle>
                 <TabMenu menuItems={worksItems}/>
                 <FlexWrapper justify='center' align='center' wrap='wrap'>
-                    <Work title='Social Network' src={socialImg} text={lorem}></Work>
-                    <Work title='Timer' src={timerImg} text={lorem}></Work>
+                    {workData.map((work, index) => {
+                        return <Work key={index} title={work.title} src={work.src} text={work.text}></Work>
+                    })}
                 </FlexWrapper>
             </Container>
-        </StyledWorks>
+        </S.Works>
     );
 };
-
-const StyledWorks = styled.section`
-    ${FlexWrapper} {
-        gap: 30px;
-    }
-`
