3d14892452a20ed32735fe4678d19798033d4f20
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-09 21:30:49 +0300

    refactor: update Contact to React func component

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -1,60 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Button } from '../../../components/Button';
 import { Container } from '../../../components/Container';
-import { myTheme } from '../../../styles/Theme';
+import { S } from './Contact_Styles';
 
-export const Contact = () => {
+export const Contact: React.FC = () => {
     return (
-        <StyledContact>
+        <S.Contact>
             <Container>
                 <SectionTitle>Contact</SectionTitle>
-                <StyledForm>
-                    <Field placeholder='Name' />
-                    <Field placeholder='Lastname' />
-                    <Field placeholder='Write Message' as={'textarea'} />
+                <S.Form>
+                    <S.Field placeholder='Name' />
+                    <S.Field placeholder='Lastname' />
+                    <S.Field placeholder='Write Message' as={'textarea'} />
                     <Button type='submit'>Send Message</Button>
-                </StyledForm>
+                </S.Form>
             </Container>
-        </StyledContact>
+        </S.Contact>
     );
 };
 
-const StyledContact = styled.section`
-`
-
-const StyledForm = styled.form`
-    max-width: 540px;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 16px;
-    margin: 0 auto;
-
-    textarea {
-        resize: none;
-        height: 155px;
-    }
-`
-
-const Field = styled.input`
-    width: 100%;
-    border: 1px solid ${myTheme.colors.borderColor};
-    background-color: ${myTheme.colors.secondaryBg};
-    padding: 7px 15px;
-    font-weight: 400;
-    font-size: 12px;
-    letter-spacing: 0.05em;
-    color: ${myTheme.colors.font};
-    font-family: ${myTheme.fonts.primaryFamily};
-    &::placeholder {
-        color:  ${myTheme.colors.placeholderColor};
-        text-transform: capitalize;
-    }
-
-    &:focus-visible {
-        outline: 1px solid ${myTheme.colors.borderColor};
-    }
-`
