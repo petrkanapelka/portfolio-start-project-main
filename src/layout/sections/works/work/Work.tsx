8e5f162ae16ad866cd158d8cf02d4e228e6d30bb
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-09 21:12:53 +0300

    refactor: update Work to func component

--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Link } from '../../../../components/Link';
-import { myTheme } from '../../../../styles/Theme';
 import { Button } from '../../../../components/Button';
+import { S } from './Works_Styles';
 
 type WorkPropsType = {
     title: string
@@ -10,101 +9,19 @@ type WorkPropsType = {
     src: string
 }
 
-export const Work = (props: WorkPropsType) => {
+export const Work: React.FC<WorkPropsType> = (props: WorkPropsType) => {
     return (
-        <StyledWork>
-            <ImageWrapper>
-                <Image src={props.src} alt='demo' />
+        <S.Work>
+            <S.ImageWrapper>
+                <S.Image src={props.src} alt='demo' />
                 <Button>view project</Button>
-            </ImageWrapper>
-            <Description>
-                <Title>{props.title}</Title>
-                <Text>{props.text}</Text>
+            </S.ImageWrapper>
+            <S.Description>
+                <S.Title>{props.title}</S.Title>
+                <S.Text>{props.text}</S.Text>
                 <Link href=''>demo</Link>
                 <Link href=''>code</Link>
-            </Description>
-        </StyledWork>
+            </S.Description>
+        </S.Work>
     );
 };
-
-
-const StyledWork = styled.div`
-    border-color: ${myTheme.colors.secondaryBg};
-    max-width: 540px;
-    width: 330px;
-    flex-grow: 1;
-
-    ${Link} {
-        padding: 10px 0;
-
-        & + ${Link} {
-            margin-left: 20px;
-        }
-    }
-
-    @media ${myTheme.media.desktop}{
-        max-width: 540px;
-    }
-`
-const Description = styled.div`
-    padding: 25px 20px;
-`
-
-const ImageWrapper = styled.div`
-    position: relative;
-
-    ${Button} {
-        transform: scale(0);
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%) scale(0);
-        &::before {
-            width: 100%;
-            height: 100%;
-        }
-    }
-
-    &::before {
-        content: "";
-        position: absolute;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        top: 0;
-        backdrop-filter: blur(8px);
-        background: rgba(0, 0, 0, 0.3);
-        opacity: 0;
-    }
-
-    &:hover {
-        &::before {
-            opacity: 1;
-        }
-
-        ${Button} {
-            transform: translate(-50%, -50%) scale(1);
-        }
-    }
-
-    @media ${myTheme.media.tablet} {
-        &::before {
-            opacity: 1;
-        }
-
-        ${Button} {
-            transform: translate(-50%, -50%) scale(1);
-        }
-    }
-`
-
-const Image = styled.img`
-    width: 100%;
-    height: 260px;
-    object-fit: cover;
-`
-const Title = styled.h3``
-
-const Text = styled.p`
-    margin: 14px 0 24px;
-`
