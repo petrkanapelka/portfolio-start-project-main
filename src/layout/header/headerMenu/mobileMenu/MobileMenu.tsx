7526a4824c88e139093df024fbe51da42e5e02cb
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-16 21:35:04 +0300

    feat: Add toggle functionality to MobileMenu component with useState.

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,15 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu } from '../menu/Menu';
 import { S } from '../HeaderMenu_Styles';
 
 export const MobileMenu: React.FC<{ menuItems: Array<string> }> = (props: { menuItems: Array<string> }) => {
+    const [ menuIsOpen, setMenuIsOpen ] = useState(false);
+    const onBurgerBtnClick = () => { setMenuIsOpen(!menuIsOpen) }
     return (
         <S.MobileMenu>
-            <S.BurgerButton isOpen={false}>
+            <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerBtnClick}>
                 <span></span>
             </S.BurgerButton>
-            <S.MobileMenuPopup isOpen={false}>
+            <S.MobileMenuPopup isOpen={menuIsOpen} onClick={()=> {setMenuIsOpen(false)}}>
                 <Menu menuItems={props.menuItems} />
             </S.MobileMenuPopup>
         </S.MobileMenu>
