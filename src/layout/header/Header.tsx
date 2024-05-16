bf57a305abe0ed45afd47a586870246655cbf28f
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-17 00:40:55 +0300

    refactor(header): simplify rendering logic for menu based on window width
    - Removed passing `menuItems` prop to `MobileMenu` and `DesktopMenu` components from `Header`.
    - Simplified rendering logic in `Header` component by checking window width against a breakpoint.

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -7,8 +7,6 @@ import { DesktopMenu } from './headerMenu/desktopMenu/DesktopMenu';
 import { MobileMenu } from './headerMenu/mobileMenu/MobileMenu';
 import { S } from './Header_Styles';
 
-const navItems = ['Home', 'Skills', 'Works', 'Testimony', 'Contacts']
-
 export const Header: React.FC = () => {
     const [width, setWidth] = React.useState(window.innerWidth);
     const breakpoint = 768;
@@ -26,8 +24,8 @@ export const Header: React.FC = () => {
                 <FlexWrapper justify='space-between' align='center'>
                     <Logo />
 
-                    {width < breakpoint ? <MobileMenu menuItems={navItems} />
-                                        : <DesktopMenu menuItems={navItems} />}
+                    {width < breakpoint ? <MobileMenu />
+                                        : <DesktopMenu />}
 
                 </FlexWrapper>
             </Container>
