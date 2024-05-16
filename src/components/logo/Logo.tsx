1116aa1980ce38c65b4bec4a1f1f5d1a3cf8627c
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-17 00:43:34 +0300

    feat(logo): add scroll to top functionality
    - Updated the Logo component to include a click event handler that scrolls the page to the top when clicked, using the 'animateScroll.scrollToTop()' function from the 'react-scroll' library.

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { Icon } from '../icon/Icon';
+import { animateScroll } from 'react-scroll';
 
 export const Logo: React.FC = () => {
     return (
-        <a href="">
+        <a onClick={()=> {animateScroll.scrollToTop()}}>
             <Icon iconID='code' width='40' height='40'/>
         </a>
     );
